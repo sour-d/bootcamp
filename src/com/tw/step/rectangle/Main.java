e12deffa7be79fefb72856b33ad804a35e9ddcfd
sour-d <36912347+sour-d@users.noreply.github.com>
2022-09-14 11:19:12 +0530

    Inital commit

---
+++ src/com/tw/step/rectangle/Main.java
@@ -0,0 +1,7 @@
+package com.tw.step.rectangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+    }
+}
---
+++ src/com/tw/step/rectangle/Rectangle.java
@@ -0,0 +1,16 @@
+package com.tw.step.rectangle;
+
+public class Rectangle {
+    private final double length;
+    private final double width;
+
+    public Rectangle(double length, double width) {
+
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area(){
+
+    }
+}

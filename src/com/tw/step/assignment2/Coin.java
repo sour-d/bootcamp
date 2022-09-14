859380bd368ded90b6ccf0da3e93c735d3a52121
sour-d <36912347+sour-d@users.noreply.github.com>
2022-09-14 14:43:35 +0530

    modified Coin entity and added chance for two coin toss

--- src/com/tw/step/assignment2/Coin.java
+++ src/com/tw/step/assignment2/Coin.java
@@ -1,9 +1,24 @@
 package com.tw.step.assignment2;
 
 public class Coin {
-    public static double possibilityOfTail() {
-        double numberOfTail = 1.0;
-        double numberOfPossibilities = 2.0;
-        return numberOfTail / numberOfPossibilities;
+    private final int totalHeads;
+    private final int totalTails;
+
+    public Coin(int totalHeads, int totalTails) {
+        this.totalHeads = totalHeads;
+        this.totalTails = totalTails;
+    }
+
+    public double possibilityOfTail() {
+        double numberOfPossibilities = this.totalTails + this.totalHeads;
+        return this.totalTails / numberOfPossibilities;
+    }
+
+    public double possibilityOfNotGettingTail() {
+        return 1 - this.possibilityOfTail();
+    }
+
+    public double possibilityOfTailWithOtherCoin(Coin coin) {
+        return this.possibilityOfTail() * coin.possibilityOfTail();
     }
 }
--- test/com/tw/step/assignment2/CoinTest.java
+++ test/com/tw/step/assignment2/CoinTest.java
@@ -7,7 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class CoinTest {
 
     @Test
-    void possibilityOfTail() {
-        assertEquals(0.5, Coin.possibilityOfTail(), 0.1);
+    void shouldReturnChanceOfTail() {
+        Coin coin = new Coin(5, 5);
+        assertEquals(0.5, coin.possibilityOfTail(), 0.1);
+    }
+
+    @Test
+    void shouldReturnChanceOfNotGettingTail() {
+        Coin coin = new Coin(5, 5);
+        assertEquals(0.5, coin.possibilityOfNotGettingTail(), 0.1);
+    }
+
+    @Test
+    void shouldReturnChanceForTwoCoin() {
+        Coin coin = new Coin(5, 5);
+        Coin coin1 = new Coin(5, 5);
+        assertEquals(0.25, coin.possibilityOfTailWithOtherCoin(coin1), 0.1);
     }
 }
